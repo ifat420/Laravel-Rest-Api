9914e46e51f6bbd149676c3af2c614c520aad2a1
ifat <ifat802@gmail.com>
2020-12-29 13:37:12 +0600

    :bug: exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,5 +41,45 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($request->is('api*')) {
+            if ($e instanceof ValidationException) {
+                return response([
+                    'status' => 'error',
+                    'errors' => $e->errors()
+                ], 422);
+            }
+
+            if ($e instanceof AuthorizationException) {
+                return response([
+                    'status' => 'error',
+                    'errors' => $e->getMessage()
+                ], 403);
+            }
+
+            if ($e instanceof ModelNotFoundException ||
+                $e instanceof NotFoundHttpException) {
+                return response([
+                    'status' => 'error',
+                    'error' => 'Resource Not found'
+                ], 404);
+            }
+
+            if ($e instanceof AuthenticationException) {
+                return response([
+                    'status' => 'error',
+                    'error' => $e->getMessage()
+                ]);
+            }
+
+            return response(['status' => 'Error', 'error' => 'Something Went Wrong'], 500);
+        }
+
+
+
+        parent::render($request, $e);
     }
 }
