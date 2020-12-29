dc870cfb0d919937b0c824324972d426fb7b4f7f
ifat <ifat802@gmail.com>
2020-12-29 13:34:40 +0600

    :sparkles: ceate project controller

---
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+use App\Http\Resources\Project as ProjectResource;
+use App\Http\Resources\ProjectCollection;
+use App\Http\Requests\ProjectRequest;
+
+class ProjectsController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Project::class, 'project');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $projects = Project::where('user_id', auth()->user()->id)
+            ->paginate();
+        return new ProjectCollection($projects);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProjectRequest $request)
+    {
+        //
+        $project = auth()->user()->projects()->create($request->all());
+        return new ProjectResource($project);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        //
+        return new ProjectResource($project);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProjectRequest $request, Project $project)
+    {
+        //
+        $project->update($request->all());
+        return new ProjectResource($project);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return [ 'status' => 'Ok' ];
+    }
+}
